41b3b0397711814798e0a5810958c456deab59f1
CardboardPL <mrplwaschilling@gmail.com>
2024-10-08 23:10:20 +0800

    Update calculator.js
    In this commit:
    - Updated calculator.js to integrate the calculateDateFromDate and validateDateString function to the event listener for the submit button.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,7 @@
 import { extractFormData, isSubmitButton, isInputElem } from './utils/form.js';
 import { formatValue } from './utils/format.js';
 import { validateDateString } from './utils/validate.js';
+import { calculateDateFromDate } from './utils/date.js';
 
 document.addEventListener('click', (e) => {
   const clickedElem = e.target;
@@ -12,17 +13,26 @@ document.addEventListener('click', (e) => {
     const day = formData.dayOfBirth;
     const month = formData.monthOfBirth;
     const year = formData.yearOfBirth;
+    const dateString = `${month}/${day}/${year}`;
     let isValid = true;
 
-    for (const data in formData) {
-      const currentData = formData[data];
-      const dataWrapperElem = document.querySelector(`.js-input[name="${data}"]`).closest('.js-fieldset-input-wrapper');
+    const dateStringValidity = validateDateString(dateString);
 
-      if (currentData.trim() === '') {
-        dataWrapperElem.classList.add('invalid');
-        isValid = false;
-      } else {
-        dataWrapperElem.classList.remove('invalid');
+    if (dateStringValidity.status) {
+      const age = calculateDateFromDate(dateString)
+    } else {
+      for (const data in formData) {
+        const currentData = formData[data];
+        const dataWrapperElem = document.querySelector(`.js-input[name="${data}"]`).closest('.js-fieldset-input-wrapper');
+        const dataType = dataWrapperElem.dataset.type;
+        const dataTypeValidity = dateStringValidity[`is${dataType}Valid`];
+  
+        if (currentData.trim() === '' || !dataTypeValidity.isValidResponse()) {
+          dataWrapperElem.classList.add('invalid');
+          isValid = false;
+        } else {
+          dataWrapperElem.classList.remove('invalid');
+        }
       }
     }
   }
